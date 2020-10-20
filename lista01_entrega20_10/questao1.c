a357af8ef515cf82f188feb3a88a33090ad674b1
Juan Alencar <juan.mr.alencar2002@gmail.com>
2020-10-20 19:29:11 +0000

    Questão 1 - Lista 1 (20/10/2020)

---
+++ lista01_entrega20_10/questao1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// LISTA DE EXERCÍCIOS - LABORATÓRIO DE DESENVOLVIMENTO DE ALGORÍTMO
+// ALUNO = JUAN DIEGO MEDEIROS ALENCAR
+
+// QUESTÃO 1 
+/*
+	Ler um número e exibir a frase: “o número digitado foi: ” e logo após esta frase apresentar
+	o valor do número lido.
+*/
+
+
+int main()
+{
+	//declarar as variaveis
+	int num;
+	
+	//solicitar um numero
+	printf("digite um numero:\n");
+	
+	//ler o numero
+	scanf("%d", &num);
+	
+	//mostrar o numero
+	printf("o numero digitado foi: %d", num);
+	
+	return 0;
+}
+
